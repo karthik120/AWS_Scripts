6fb3898ad24a02254e2adf9eb46685e2dc4d4387
Karthikeyan Sundharrajan (axsundha) <axsundha@cisco.com>
2018-06-25 18:22:09 +0530

    Script to delete CloudWatch Alarms

---
+++ CloudWatch_Alarm_deletion.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script to delete all the alarms in Cloudwatch. Set approprite region in .aws/config file before executing this file
+# Authon : Karthikeyan Sundharrajan ( email: karthik120@gmail.com )
+region=`cat .aws/config | grep region | cut -c 10-`
+aws cloudwatch describe-alarms > us_${region}_alarms.json
+cat us_${region}_alarms.json | jq --compact-output '.MetricAlarms[] | {Alarmname: .AlarmName}' | cut -c 15- | sed 's/.\{2\}$//' > US_${region}_Alarms.txt
+file=/Users/axsundha/US_${region}_Alarms.txt
+while IFS= read -r line
+do
+  aws cloudwatch delete-alarms --alarm-names `echo $line`
+done <"$file"
